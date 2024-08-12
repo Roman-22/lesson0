a7bd66610803d61f04c0326f240ec0ca5b9d10a9
Roman-22 <zaruba195@yandex.ru>
2024-08-12 22:42:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print(9 ** 0.5 * 5)                                                    # "1st program"
+print(9.99 > 9.98 and 1000 != 1000.1)                                  # "2st program"
+print((2 + 2 * 2) == 2 * (2+2))                                        # "3rd program"
+print('123.456' and float (123.456) * 10 % 10)                         #"4th program"
